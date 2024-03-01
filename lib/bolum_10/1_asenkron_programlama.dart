9b2c426f10e7a5e757de19bbc79ba735eb175db2
omer <omermenekse@gmail.com>
2024-03-01 22:48:48 +0300

    yeni

---
+++ lib/bolum_10/1_asenkron_programlama.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  //git kayıtlarını unutma
+  print("anne çocuğu ekmek almaya yollar");
+  uzunSurenIslem();
+  print("peynir zeytin hazırlar");
+  print("kahvaltı hazır");
+}
+
+//flutter single thread bir yapıda çalışır.
+//yani işler birbiri ile peş peşe çalışır
+
+//bu metod asenkron çalışma için
+void uzunSurenIslem() {
+  print("çocuk ekmek almak için evden çıkar");
+  Future.delayed(Duration(seconds: 10), () {
+    print("çocuk ekmekle eve girer");
+  });
+}
+
+/*
+bu metod senkron çalışma için
+void uzunSurenIslem() {
+  print("çocuk ekmek almak için evden çıkar");
+  sleep(Duration(seconds: 10));
+  print("çocuk ekmekle eve girer");
+}*/
+
+
+/*
+bu şekilde çalışma senkron bir çalışmadır. bu çalışmada bir işlem
+bitmeden diğer işleme geçilmezde bu da uygulamaaın yavaş çalışmasına neden olur. 
+bunun için asenkron çalışma uygulanır
+*/
